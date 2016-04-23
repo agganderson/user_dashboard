dc506682043d5ddc1f6937d74bba61eb8bf10e8a
Alexandra Anderson <aliganderson@gmail.com>
2016-04-22 17:49:08 -0700

    userCtrl, dashCtrl, userFac working. Name is displaying on dashboard.html

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,10 +1,18 @@
 var users = require('./../controllers/users.js');
-var topics = require('./../controllers/topics.js');
-var comments = require('./../controllers/comments.js');
-var answers = require('./../controllers/answers.js');
+// var topics = require('./../controllers/topics.js');
+// var comments = require('./../controllers/comments.js');
+// var answers = require('./../controllers/answers.js');
 
 
 
 module.exports = function(app){
-	
+	app.get('/users', function(req, res){
+		console.log('hit users get route');
+		users.index(req, res);
+	});
+
+	app.post('/users', function(req, res){
+		console.log('hit users post route');
+		users.create(req, res);
+	})
 }
---
+++ server/controllers/users.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+module.exports = (function(){
+	return {
+		index: function(req, res){
+			User.find({}, function(err, results){
+				if(err){
+					console.log('ERROR ERROR', results);
+				}
+				else {
+					console.log('Got dem users');
+					res.json(results);
+				}
+			});
+		},
+		create: function(req, res){
+			var newUser = new User({name: req.body.name});
+			newUser.save(function(err, results){
+				if(err){
+					console.log('GET THE DEFIBRILLATOR', results);
+				}
+				else {
+					console.log('SAVED THEM');
+					res.json(results);
+				}
+			});
+		}
+	}
+})();
